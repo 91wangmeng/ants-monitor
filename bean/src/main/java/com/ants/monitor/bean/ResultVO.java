c288e2f6398f4e89c83ef4ff78d0e3f5f46f1d57
wangmm <Qq64192178>
2017-03-31 15:58:36 +0800

    Initial commit

---
+++ bean/src/main/java/com/ants/monitor/bean/ResultVO.java
@@ -0,0 +1,78 @@
+package com.ants.monitor.bean;
+import lombok.Data;
+
+/**
+ * Created by zxg on 15/11/2.
+ */
+@Data
+public class ResultVO {
+
+    private boolean success;
+    private String code = "000";
+    private String msg = "";
+    private Object data;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public static ResultVO wrapSuccessfulResult() {
+        ResultVO vo = new ResultVO();
+        vo.setSuccess(true);
+        return vo;
+    }
+    public static ResultVO wrapSuccessfulResult(Object data) {
+        ResultVO vo = new ResultVO();
+        vo.setSuccess(true);
+        vo.setData(data);
+        return vo;
+    }
+
+    public static ResultVO wrapErrorResult(String msgStr) {
+        ResultVO vo = new ResultVO();
+        vo.setSuccess(false);
+
+        vo.setMsg(msgStr);
+
+        return vo;
+    }
+
+
+    public static ResultVO wrapErrorResult(String code,String msgStr) {
+        ResultVO vo = new ResultVO();
+        vo.setCode(code);
+        vo.setSuccess(false);
+        vo.setMsg(msgStr);
+
+        return vo;
+    }
+
+}
